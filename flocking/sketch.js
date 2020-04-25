8bbb87e13aec50cb8e42d788078d52b5fa771857
Anindya Kundu <anindya.k22@outlook.com>
2020-04-25 14:47:29 +0530

    Set canvas fullscreen

--- flocking/sketch.js
+++ flocking/sketch.js
@@ -1,20 +1,29 @@
-const flock = [];
-
-let alignSlider, cohesionSlider, separationSlider;
+let flock = [];
 
 function setup() {
-  createCanvas(640, 360);
-  for (let i = 0; i < 100; i++) {
-    flock.push(new Boid());
-  }
+    createCanvas(windowWidth, windowHeight);
+    createBoids();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    createBoids();
 }
 
 function draw() {
-  background(51);
-  for (let boid of flock) {
-    boid.edges();
-    boid.flock(flock);
-    boid.update();
-    boid.show();
-  }
+    background(51);
+    for (let boid of flock) {
+        boid.edges();
+        boid.flock(flock);
+        boid.update();
+        boid.show();
+    }
+}
+
+function createBoids() {
+    flock = [];
+    let boidCount = floor((100 * width * height) / (640 * 360));
+    for (let i = 0; i < min(200, boidCount); i++) {
+        flock.push(new Boid());
+    }
 }
